cb0fa2583aa17f0cf8ab65d2f486f26d9feeaf4f
jzeyen <jzeyen35@gmail.com>
2022-01-28 09:39:28 +0100

    Create show temprature
    show Temperature
    show Wind speed
    show description
    show city
    show humidity

--- src/app.js
+++ src/app.js
@@ -1,1 +1,34 @@
-alert("Hello you!");
+let currentTime = new Date();
+let weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let currentWeekday = weekdays[currentTime.getDay()];
+let currentHour = currentTime.getHours();
+let currentMinutes = currentTime.getMinutes();
+document.querySelector(
+  "#currentTime"
+).innerHTML = `${currentWeekday} ${currentHour}:${currentMinutes} `;
+
+function showTemperature(response) {
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+let apiKey = "751992fab258275d0488850c6c939811";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showTemperature);
